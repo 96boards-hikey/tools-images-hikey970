7ea233bb060de5d8947a0019c3a45f20a4cfdb62
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-07-03 17:48:09 +0530

    hisi-idt.py: Modify script to add support for Python3
    This commit modifies the hisi-idt.py script to add Python3 support without
    breaking the Python2 compatibility.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- hisi-idt.py
+++ hisi-idt.py
@@ -100,7 +100,7 @@ class bootdownload(object):
 
     def calc_crc(self, data, crc=0):
         for char in data:
-            crc = ((crc << 8) | ord(char)) ^ self.crctable[(crc >> 8) & 0xff]
+            crc = ((crc << 8) | char) ^ self.crctable[(crc >> 8) & 0xff]
         for i in range(0,2):
             crc = ((crc << 8) | 0) ^ self.crctable[(crc >> 8) & 0xff]
         return crc & 0xffff
@@ -116,9 +116,8 @@ class bootdownload(object):
             self.s.flushInput()
             try:
                 ack = self.s.read()
-                if len(ack) == 1:
-                    if ack == chr(0xaa):
-                        return None
+                if ack == b'\xaa':
+                     return None
             except:
                 return None
 
@@ -143,36 +142,35 @@ class bootdownload(object):
         self.headframe[self.chip][10] = (address>>8)&0xff
         self.headframe[self.chip][11] = (address)&0xff
 
-        data = array.array('B', self.headframe[self.chip]).tostring()
+        data = bytearray(self.headframe[self.chip])
         crc = self.calc_crc(data)
 
-        data += chr((crc >> 8)&0xff)
-        data += chr(crc&0xff)
+        data.append((crc >> 8)&0xff)
+        data.append(crc&0xff)
 
         self.sendframe(data,16)
 
 
     def senddataframe(self,seq,data):
         self.s.timeout = 0.15
-        head = chr(0xDA)
-        head += chr(seq&0xFF)
-        head += chr((~seq)&0xFF)
-
+        head = bytearray(b'\xDA')
+        head.append(seq&0xFF)
+        head.append((~seq)&0xFF)
         data = head + data
 
         crc = self.calc_crc(data)
-        data += chr((crc >> 8)&0xff)
-        data += chr(crc&0xff)
+        data.append((crc >> 8)&0xff)
+        data.append(crc&0xff)
 
         self.sendframe(data,32)
 
     def sendtailframe(self,seq):
-        data = chr(0xED)
-        data += chr(seq&0xFF)
-        data += chr((~seq)&0xFF)
+        data = bytearray(b'\xED')
+        data.append(seq&0xFF)
+        data.append((~seq)&0xFF)
         crc = self.calc_crc(data)
-        data += chr((crc >> 8)&0xff)
-        data += chr(crc&0xff)
+        data.append((crc >> 8)&0xff)
+        data.append(crc&0xff)
 
         self.sendframe(data,16)
 
